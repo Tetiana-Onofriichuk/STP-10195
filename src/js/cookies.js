59fb2bc8a65daaae22836c0e99a7db630058297f
Tetiana <Korostijtatiana@gmail.com>
2025-08-23 20:13:45 +0300

    add modal coocies

---
+++ src/js/cookies.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const banner = document.getElementById('cookies');
+  const btnAccept = document.getElementById('acceptCookies');
+  const btnDecline = document.getElementById('declineCookies');
+
+  if (localStorage.getItem('cookiesAccepted')) {
+    banner.style.display = 'none';
+  }
+
+  btnAccept.addEventListener('click', () => {
+    localStorage.setItem('cookiesAccepted', 'true');
+    banner.style.display = 'none';
+  });
+
+  btnDecline.addEventListener('click', () => {
+    banner.style.display = 'none';
+  });
+});
